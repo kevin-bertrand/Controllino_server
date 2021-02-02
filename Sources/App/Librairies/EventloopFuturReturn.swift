545c3dc9bc38dbdf48f1030935cd44f365f0c8a6
Kevin Bertrand <k.bertrand@desyntic.com>
2021-02-02 14:17:34 +0100

    Create EventloopFuture function to return an error when the request failed

---
+++ Sources/App/Librairies/EventloopFuturReturn.swift
@@ -0,0 +1,16 @@
+//
+//  EventloopFuturReturn.swift
+//  
+//
+//  Created by Kevin Bertrand on 02/02/2021.
+//
+
+import Fluent
+//import FluentSQL
+import Vapor
+
+struct EventLoopFutureReturn {
+    func errorHttpStatus(on req: Request, withError error: HTTPStatus) -> EventLoopFuture<HTTPStatus> {
+        return User.query(on: req.db).first().transform(to: error)
+    }
+}
